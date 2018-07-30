03c96ccb581e7d76d5c24ea9abd71c843add4d3c
Jyupanqui <jhonjy95@gmail.com>
2018-07-29 21:11:40 -0500

    Initial commmit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+export default {
+    appName: 'jywebmusic',
+    apiKey: '0886efd29f8edb1501679fbba0f47736',
+    secret: '864e21933962ed17ed88b02a37f4ce89',
+    registeredTo: 'jhoucito'
+}
+/*Nombre de la aplicación	jywebmusic
+Clave API	0886efd29f8edb1501679fbba0f47736
+Secreto compartido	864e21933962ed17ed88b02a37f4ce89
+Registrado a	jhoucito*/
